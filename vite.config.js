c7b911acaf408a88a8ae327d60f909baa9d6e2ff
nicolasgmack <nicolasgmack@gmail.com>
2025-10-06 08:50:08 -0300

    fix: quando da f5 em uma rota navegada não dá erro

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // garante que qualquer rota caia no index.html
+    fs: {
+      strict: false
+    }
+  },
+  preview: {
+    // no modo preview (após build), força fallback também
+    port: 4173,
+    strictPort: true
+  },
+  appType: 'spa' // <-- essa é a chave p/ Vite tratar como SPA
 })
